7fd16f1837ef45b7f3e0649408639b206a1efb10
lafloba <90605843+lafloba@users.noreply.github.com>
2021-09-30 16:37:29 +0200

    Projectile

--- Assets/ScriptsCharacter/RubyController.cs
+++ Assets/ScriptsCharacter/RubyController.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class RubyController : MonoBehaviour
 {
    Rigidbody2D rigidbody2d;
+   public GameObject (projectilePrefab);
     
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        projectilePrefab = GetComponent<GameObject>();
     }
 
     // Update is called once per frame
@@ -23,5 +25,26 @@ public class RubyController : MonoBehaviour
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Launch();
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Launch();
+        }
     }
+
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+
+        animator.SetTrigger("Launch");
+    }
+
+
 }
