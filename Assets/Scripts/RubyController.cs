9957295edbc6e6bd61eb3c6c2583444828e9187f
ausiasxp <al397425@uji.es>
2021-09-30 17:21:45 +0200

    Changes

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,12 +4,34 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+<<<<<<< Updated upstream
    Rigidbody2D rigidbody2d;
+=======
+    public float speed = 3.0f;
+    
+    public int maxHealth = 5;
+   
+    
+    public int health {  get { return currentHealth; }}
+    int currentHealth;
+    
+    public float timeInvincible = 2.0f;
+    bool isInvincible;
+    float invincibleTimer;
+    
+    
+    Rigidbody2D rigidbody2d;
+>>>>>>> Stashed changes
     
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+<<<<<<< Updated upstream
+=======
+
+        currentHealth = maxHealth;
+>>>>>>> Stashed changes
     }
 
     // Update is called once per frame
@@ -24,4 +46,23 @@ public class RubyController : MonoBehaviour
 
         rigidbody2d.MovePosition(position);
     }
+<<<<<<< Updated upstream
+=======
+
+    public void ChangeHealth(int amount)
+    {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+            
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+        
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+>>>>>>> Stashed changes
 }
