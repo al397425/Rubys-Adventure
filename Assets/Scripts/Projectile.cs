de38095da0313a20a3bfd4b0a65fbffb374f2f18
lafloba <90605843+lafloba@users.noreply.github.com>
2021-09-30 10:44:57 +0200

    Camera 1

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
-    Rigidbody2D rigidbody2d;
+    
 {
+    Rigidbody2D rigidbody2d;
     // Start is called before the first frame update
     void Start()
     {
