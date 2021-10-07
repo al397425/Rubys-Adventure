591745bca2eefb0691a5562417bf7f3f54cde566
lafloba <90605843+lafloba@users.noreply.github.com>
2021-10-07 16:43:27 +0200

    Update RubyController.cs

--- Assets/ScriptsCharacter/RubyController.cs
+++ Assets/ScriptsCharacter/RubyController.cs
@@ -7,26 +7,19 @@ public class RubyController : MonoBehaviour
     public float speed = 3.0f;
 
     public int maxHealth = 5;
-    public float timeInvincible = 2.0f;
-    public GameObject projectilePrefab;
 
     public int health { get { return currentHealth; } }
     int currentHealth;
-    bool isInvincible;
-    float invincibleTimer;
 
     Rigidbody2D rigidbody2d;
 
-    Animator animator;
-    Vector2 lookDirection = new Vector2(1, 0);
-
-    // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
 
         currentHealth = maxHealth;
+
+
     }
 
     // Update is called once per frame
@@ -35,21 +28,9 @@ public class RubyController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        Vector2 move = new Vector2(horizontal, vertical);
-
-        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
-        {
-            lookDirection.Set(move.x, move.y);
-            lookDirection.Normalize();
-        }
-
-        animator.SetFloat("Look X", lookDirection.x);
-        animator.SetFloat("Look Y", lookDirection.y);
-        animator.SetFloat("Speed", move.magnitude);
-
         Vector2 position = rigidbody2d.position;
-
-        position = position + move * speed * Time.deltaTime;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
 
@@ -58,20 +39,6 @@ public class RubyController : MonoBehaviour
             invincibleTimer -= Time.deltaTime;
             if (invincibleTimer < 0)
                 isInvincible = false;
-        }
-
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            Launch();
-        }
-
-        if (Input.GetKeyDown(KeyCode.X))
-        {
-            RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
-            if (hit.collider != null)
-            {
-                Debug.Log("Raycast has hit the object " + hit.collider.gameObject);
-            }
         }
     }
 
@@ -85,19 +52,4 @@ public class RubyController : MonoBehaviour
             isInvincible = true;
             invincibleTimer = timeInvincible;
         }
-
-        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-
-        UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
     }
-
-    void Launch()
-    {
-        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
-
-        Projectile projectile = projectileObject.GetComponent<Projectile>();
-        projectile.Launch(lookDirection, 300);
-
-        animator.SetTrigger("Launch");
-    }
-}
