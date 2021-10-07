1cf5c96b1019d8997a85407574104efc59b0ad06
lafloba <90605843+lafloba@users.noreply.github.com>
2021-10-07 11:42:09 +0200

    Projectile

--- Assets/ScriptsCamera/Projectile.cs
+++ Assets/ScriptsCamera/Projectile.cs
@@ -3,24 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
-    
 {
     Rigidbody2D rigidbody2d;
-    // Start is called before the first frame update
-    void Start()
+
+    void Awake()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
-    }
-
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        //we also add a debug log to know what the projectile touch
-        Debug.Log("Projectile Collision with " + other.gameObject);
-        Destroy(gameObject);
     }
 
     public void Launch(Vector2 direction, float force)
     {
         rigidbody2d.AddForce(direction * force);
     }
+
+    void Update()
+    {
+        if (transform.position.magnitude > 1000.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        /*EnemyController e = other.collider.GetComponent<EnemyController>();
+        if (e != null)
+        {
+            e.Fix();
+        }*/
+
+        Destroy(gameObject);
+    }
 }
+
+
--- Assets/ScriptsCharacter/RubyController.cs
+++ Assets/ScriptsCharacter/RubyController.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
-   Rigidbody2D rigidbody2d;
-   public GameObject projectilePrefab;
+    public float speed = 3.0f;
+
+    public int maxHealth = 5;
+    public float timeInvincible = 2.0f;
+    public GameObject projectilePrefab;
+
+    public int health { get { return currentHealth; } }
+    int currentHealth;
+    bool isInvincible;
+    float invincibleTimer;
+
+    Rigidbody2D rigidbody2d;
+
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
-        projectilePrefab = GetComponent<GameObject>();
+        animator = GetComponent<Animator>();
+
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -19,21 +34,52 @@ public class RubyController : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + 3.0f* horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+
+        position = position + move * speed * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
 
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
+
         if (Input.GetKeyDown(KeyCode.C))
         {
             Launch();
         }
-        if (Input.GetKeyDown(KeyCode.C))
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        if (amount < 0)
         {
-            Launch();
+            if (isInvincible)
+                return;
+
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
         }
+
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 
     void Launch()
@@ -45,6 +91,4 @@ public class RubyController : MonoBehaviour
 
         animator.SetTrigger("Launch");
     }
-
-
 }
